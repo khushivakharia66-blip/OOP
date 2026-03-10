e088831e647b78fe8d2c24bdb33ac73dc3a58716
khushivakharia66-blip <khushivakharia66@gmail.com>
2026-03-10 08:54:49 +0530

    Add files via upload

---
+++ CollegeAdmission.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class College {
+    String collegeName;
+
+        College(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    
+    class Admission {
+        String studentName;
+        String course;
+
+                void acceptDetails() {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter Student Name: ");
+            studentName = sc.nextLine();
+
+            System.out.print("Enter Course Enrolled: ");
+            course = sc.nextLine();
+        }
+
+               void displayDetails() {
+            System.out.println("\n--- Admission Details ---");
+            System.out.println("College Name: " + collegeName);
+            System.out.println("Student Name: " + studentName);
+            System.out.println("Course: " + course);
+        }
+    }
+
+    public static void main(String[] args) {
+
+                College college = new College("ABC Engineering College");
+
+        
+        College.Admission admission = college.new Admission();
+
+        
+        admission.acceptDetails();
+        admission.displayDetails();
+    }
+}
