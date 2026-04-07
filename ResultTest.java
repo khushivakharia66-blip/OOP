a389a3c35923e7bd2bafea24bcc92b59dafbf3e6
khushivakharia66-blip <khushivakharia66@gmail.com>
2026-04-07 10:43:18 +0530

    Add files via upload

---
+++ ResultTest.java
@@ -0,0 +1,20 @@
+interface Classify {
+    String getDivision(double average);
+}
+
+class Result implements Classify {
+    public String getDivision(double average) {
+        if (average >= 60) {
+            return "First Division";
+        } else {
+            return "No First Division";
+        }
+    }
+}
+
+public class ResultTest {
+    public static void main(String[] args) {
+        Result r = new Result();
+        System.out.println(r.getDivision(70));
+    }
+}
